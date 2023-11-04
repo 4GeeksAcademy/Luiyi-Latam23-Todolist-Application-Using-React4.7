af0769f703d4488631a878d0af6dd17b7be4cd27
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-04 15:39:04 +0000

    css adjusted

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export const Todos = () => {
   return (
     <div className="container">
-      <div className="ulContainer text-center">TODO'S LIST</div>
-      <ul className="liContainer text-left">
+      <div className="ulContainer">
+        <h1>TODO'S LIST</h1>
+      </div>
+      <ul className="items">
         <li>Make the bed</li>
         <li>Take out the trash</li>
         <li>Go to the Gym</li>
