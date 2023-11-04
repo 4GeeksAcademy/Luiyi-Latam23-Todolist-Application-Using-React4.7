a1d7ce3bdf9a76000617311c402b69d1f6909768
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-04 16:53:47 +0000

    input added

--- src/js/component/todos.jsx
+++ src/js/component/todos.jsx
@@ -3,9 +3,19 @@ import React from "react";
 export const Todos = () => {
   return (
     <div className="container">
-      <div className="ulContainer">
+      <div>
         <h1>TODO'S LIST</h1>
       </div>
+      <form className="item-form">
+        <input
+          type="text"
+          className="form-input"
+          id="item-input"
+          name="item"
+          placeholder="Enter Task!"
+          autoComplete="off"
+        />
+      </form>
       <ul className="items">
         <li>Make the bed</li>
         <li>Take out the trash</li>
